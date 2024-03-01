88da032361b31997961ee40041dad77e35567df5
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2024-02-29 19:35:27 -0800

    Created additional tests
    - Created random row na test
    - Created data type test

--- code/test/clean_data_test.py
+++ code/test/clean_data_test.py
@@ -1,11 +1,36 @@
 import pandas as pd
+import numpy as np
 
-def test_no_na_rows():
-    # Read the cleaned data from the CSV file
-    data = pd.read_csv('../../data/cleaned_data.csv')
-
-    # Check if any rows contain NA values
+def test_no_na_rows(data):
     assert data.isna().sum().sum() == 0, "Some rows contain NA values."
 
-# Run the test
-test_no_na_rows()
+def test_random_row(data):
+    assert data.iloc[np.random.randint(0, len(data))].isna().sum() == 0, f"Row {random_index} contains NA values."
+
+def test_data_types(data):
+    expected_data_types = {
+        'obs': 'int64',
+        'year': 'int64',
+        'month': 'int64',
+        'day': 'int64',
+        'date': 'int64',
+        'latitude': 'float64',
+        'longitude': 'float64',
+        'zon.winds': 'float64',
+        'mer.winds': 'float64',
+        'humidity': 'float64',
+        'air': 'float64',
+        'temp.': 'float64'
+    }
+
+    for column, expected_type in expected_data_types.items():
+        assert data[column].dtype == expected_type, f"Column '{column}' has incorrect data type."
+
+def run_tests():
+    data = pd.read_csv('../../data/cleaned_data.csv')
+    test_no_na_rows(data)
+    test_random_row(data)
+    test_data_types(data)
+
+
+run_tests()
