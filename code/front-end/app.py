302b80daec58c36936fd29d7a215914d70119459
Lakshay Dang <97419965+danglakshay@users.noreply.github.com>
2024-03-10 19:38:03 -0700

    Flask integration for login page

--- code/front-end/app.py
+++ code/front-end/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('app.html')
+    return render_template('login/login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
