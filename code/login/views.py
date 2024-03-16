6a12fc2060fc0ea6d91901e10e65b1bbbf6d3774
Lakshay Dang <97419965+danglakshay@users.noreply.github.com>
2024-03-16 13:36:13 -0700

    Updated Views.py

--- code/login/views.py
+++ code/login/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 views = Blueprint('views', __name__)
 
 
-# @views.route('/')
-# def home():
-#     return "<h1> Hello World </h1>"
+@views.route('/')
+def home():
+    return "<h1> Hello World </h1>"
