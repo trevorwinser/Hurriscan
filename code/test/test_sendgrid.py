7c839290e06fb2ca3c7ed263fde2c44d3b75fe3c
Noah <noah.stasuik@gmail.com>
2024-03-17 20:09:59 -0700

    Updated testing for personal phone number and email

--- code/test/test_sendgrid.py
+++ code/test/test_sendgrid.py
@@ -1,19 +1,19 @@
-# using SendGrid's Python Library
-# https://github.com/sendgrid/sendgrid-python
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+email = input("Enter your email address: ")
+
 message = Mail(
-    from_email='noah.stasuik@gmail.com',
-    to_emails='noah.stasuik@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>andd easy to do anywhere, even with Python</strong>')
+    from_email="noah.stasuik@gmail.com",
+    to_emails=email,
+    subject='Hurriscan Alert!',
+    html_content='<strong>Warning!</strong> Hurricane Activity in your area!!')
 try:
-    sg = SendGridAPIClient(os.environ.get('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'))
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
     print(response.headers)
 except Exception as e:
-    print(e.message)
+    print(str(e)) 
--- code/test/test_twilio.py
+++ code/test/test_twilio.py
@@ -4,10 +4,12 @@ account_sid = 'AC44993aed976dd5210997b2519df5a254'
 auth_token = '834adc531af2f6b71e3d8d94c884b2fb'
 client = Client(account_sid, auth_token)
 
+phone_number = input("Enter your phone number: ")
+
 message = client.messages.create(
   from_='+19163024424',
   body='Testing Hurriscan',
-  to='+13065702634'
+  to=phone_number
 )
 
 print(message.sid)
