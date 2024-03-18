d1f67e77074540a8f530c488405179f7820c6a65
Julie Flament <juliemeflament@gmail.com>
2024-03-17 19:30:19 -0700

    Date Filter
    - added filter which filters the hurricanes based on month and year
    - initially shows all of the hurricanes until a filter is used
    - updates the heatmap after user inputs what values they want in filter

--- code/app.py
+++ code/app.py
@@ -1,81 +1,29 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, jsonify, redirect, request
 import os
 import sqlite3
+import sqlite_setup
+from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+sqlite_setup.main()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'hurriscan.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
-    
-
-    
 @app.route('/data-visualization')
 def data_visualization():
-    # Connect directly to the database
     conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
-    conn.row_factory = sqlite3.Row  # This enables column access by name: row['column_name']
+    conn.row_factory = sqlite3.Row
     cur = conn.cursor()
 
-    # Execute a query to fetch data from your table
     cur.execute('SELECT month, SUM(temp) as total_temp FROM Data GROUP BY month')
-    data = cur.fetchall()  # Fetches all rows as a list of dicts
+    data = cur.fetchall()
 
-    # Close the connection
     conn.close()
 
-    # Convert data to format suitable for visualization (e.g., lists of labels and values)
     months = [row['month'] for row in data]
     temps = [row['total_temp'] for row in data]
 
-    # Pass this data to your template
     return render_template('data_visualization.html', months=months, temps=temps)
 
-@app.route('/map-filter')
-def mapfilter():
-    # Connect directly to the database
-    conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT latitude, longitude, humidity FROM Data;")
-        rows = cursor.fetchall()
-
-        # Convert the retrieved data to JavaScript format
-        js_data = "const el_nino_data = {\n"
-        js_data += "    max: 100,\n"
-        js_data += "    data: [\n"
-
-        for row in rows:
-            js_data += f"        {{latitude: '{row[0]}', longitude: '{row[1]}', humidity: '{row[2]}'}}"
-
-            # Add comma if not the last row
-            if row != rows[-1]:
-                js_data += ",\n"
-            else:
-                js_data += "\n"
-
-        js_data += "    ]\n};"
-
-        with open(os.path.join(basedir, 'static', 'js/el_nino_data.js'), 'w') as js_file:
-            js_file.write(js_data)
-
-    except sqlite3.Error as e:
-        print("Error executing SQL statement:", e)
-
-    finally:
-        # Close the database connection
-        conn.close()
-
-    return render_template('mapfilter-temp/mapfilter.html')
-    
 @app.route('/')
 def home():
     return 'Welcome to the Home Page'
@@ -96,8 +44,147 @@ def createAcc():
 def alerts_page():
     return render_template('alerts.html')
 
-with app.app_context():
-    db.create_all()
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('admin/admin-dashboard.html')
 
+@app.route('/admin-nav-bar')
+def admin_nav_bar():
+    return render_template('admin/admin-nav-bar.html')
+
+@app.route('/users')
+def get_users():
+    conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM User')
+    rows = cur.fetchall()
+
+    users = []
+    for row in rows:
+        user = {
+            'id': row[0],
+            'username': row[1],
+            'password': row[2],
+            'email': row[3],
+            'phone': row[4],
+            'alerts_email': row[5],
+            'alerts_phone': row[6],
+            'isAdmin': row[7]
+        }
+        users.append(user)
+
+    conn.close()
+    return jsonify(users)
+
+@app.route('/delete-user/<int:user_id>')
+def delete_user(user_id):
+    try:
+        conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
+        cur = conn.cursor()
+        cur.execute('DELETE FROM User WHERE id = ?', (user_id,))
+        conn.commit()
+        
+        conn.close()
+        return redirect('/admin') # Doesn't do anything, but needed to return something ¯\_(ツ)_/¯
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/create-alert', methods=['POST'])
+def create_alert():
+    title = request.form['title']
+    text = request.form['text']
+    date = datetime.now().date()
+
+    conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
+    cur = conn.cursor()
+    cur.execute('INSERT INTO Alert (title, text, date) VALUES (?, ?, ?)', (title, text, date))
+    conn.commit()
+    cur.execute('SELECT * FROM Alert')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+
+    return redirect('/alerts') # Wrong path but will change later!
+
+@app.route('/initial-data')
+def initial_data():
+    try:
+        conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("SELECT latitude, longitude, humidity FROM Data;")
+
+        data = [dict(row) for row in cursor.fetchall()]
+
+        conn.close()
+
+        return jsonify(data), 200
+
+    except sqlite3.Error as e:
+        return f"Error {e} fetching initial data from database", 500
+
+@app.route('/map-filter')
+def mapfilter():
+    month = request.args.get('month')
+    year = request.args.get('year')
+    
+    if month and year:
+        # Convert month and year to integers
+        month = int(month)
+        year = int(year)
+        
+        # If month and year are provided, fetch filtered data from the database
+        conn = sqlite3.connect(os.path.join(basedir, 'hurriscan.db'))
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        
+        try:
+            cursor.execute("SELECT latitude, longitude, humidity FROM Data WHERE month = ? AND year = ?;", (month, year))
+            rows = cursor.fetchall()
+
+        except sqlite3.Error as e:
+            return f"Error {e} fetching filtered data from database", 500
+        finally:
+            conn.close()
+
+        # Serialize the data to JSON format
+        data = [{'latitude': row[0], 'longitude': row[1], 'humidity': row[2]} for row in rows]
+
+        # Render the mapfilter.html template and pass the JSON data as a context variable
+        return data, 200
+    else:
+        # If month and year are not provided, simply render the mapfilter.html template
+        return initial_data()
+
+@app.route('/temperature-filter')
+def temperature_filter():
+    # Handle temperature range filter
+    min_temperature = request.args.get('minTemperature')
+    max_temperature = request.args.get('maxTemperature')
+
+    if min_temperature is not None and max_temperature is not None:
+        # Apply temperature range filter
+        connection = sqlite3.connect((os.path.join(basedir, 'hurriscan.db'))) 
+        cursor = connection.cursor()
+        cursor.execute("SELECT latitude, longitude, humidity FROM Data WHERE temperature BETWEEN ? AND ?", (min_temperature, max_temperature))
+        filtered_data = cursor.fetchall()
+        connection.close()
+    else:
+        # No filters applied, return all data
+        connection = sqlite3.connect((os.path.join(basedir, 'hurriscan.db'))) 
+        cursor = connection.cursor()
+        cursor.execute("SELECT latitude, longitude, humidity FROM Data")
+        filtered_data = cursor.fetchall()
+        connection.close()
+
+    return jsonify(filtered_data)
+
+
+    return jsonify(filtered_data)
 if __name__ == '__main__':
     app.run(debug=True)
+
