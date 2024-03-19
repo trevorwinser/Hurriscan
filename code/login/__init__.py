a771cc6fdcce80815d4f3686a4c3ab8e5581febb
Lakshay Dang <97419965+danglakshay@users.noreply.github.com>
2024-03-19 02:28:48 -0700

    Attempted to integrate code into system
    Issues raised, a greater discussion for the file structure needs to be had.

--- code/login/__init__.py
+++ code/login/__init__.py
@@ -25,8 +25,9 @@ def create_app():
     from .views import views
     from .auth import auth
     
-    app.register_blueprint(views, url_prefix = '/')
+
     app.register_blueprint(auth, url_prefix = '/')
+    app.register_blueprint(views, url_prefix = '/')
     
     from .models import User
     
