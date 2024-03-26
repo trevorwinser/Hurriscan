d840de7447489becc85c6cf0f698c6ad09b88927
Julie Flament <juliemeflament@gmail.com>
2024-03-25 19:02:06 -0700

    made front end testing for predictions dashboard

--- code/test/predictions_dashboard_frontend_test.py
+++ code/test/predictions_dashboard_frontend_test.py
@@ -26,6 +26,33 @@ class TestPredictionsDashboard(unittest.TestCase):
         self.assertNotEqual(latitude_input.get_attribute("value"), "")
         self.assertNotEqual(longitude_input.get_attribute("value"), "")
         
+    def test_select_map_spot(self):
+        map_element = self.driver.find_element(By.ID, "map")
+        map_element.click()
+
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "latitude")) and
+            EC.presence_of_element_located((By.ID, "longitude"))
+        )
+
+        latitude_input = self.driver.find_element(By.ID, "latitude")
+        longitude_input = self.driver.find_element(By.ID, "longitude")
+        latitude_value = latitude_input.get_attribute("value")
+        longitude_value = longitude_input.get_attribute("value")
+
+        self.assertNotEqual(latitude_value, "")
+        self.assertNotEqual(longitude_value, "")
+        
+    def test_click_checkboxes(self):
+        north_america_checkbox = self.driver.find_element(By.ID, "north-america")
+        north_america_checkbox.click()
+
+        south_america_checkbox = self.driver.find_element(By.ID, "south-america")
+        south_america_checkbox.click()
+
+        self.assertTrue(north_america_checkbox.is_selected())
+        self.assertTrue(south_america_checkbox.is_selected())
+        
     def tearDown(self):
         self.driver.quit()
 
