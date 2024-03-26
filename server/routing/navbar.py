cd4833812e7070c6ed3554a55b76d8c52c1db7ae
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2024-03-26 09:50:47 -0700

    User Dashboard now loads map-visualization
    - Used to load outdated map

---
+++ server/routing/navbar.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, request
+import os
+
+navbar_bp = Blueprint('navbar', __name__, template_folder="../templates", static_folder="../static")
+
+@navbar_bp.route('/navbar')
+def navbar():
+    return render_template('navbar.html')
